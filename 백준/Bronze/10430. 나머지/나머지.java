b94e0ce09a166c9665f8acbd527164f773b7c117
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-04-08 21:54:59 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,20 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
  
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int A = in.nextInt();
-		int B = in.nextInt();
-		int C = in.nextInt();
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		in.close();
- 
+ 		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
 		System.out.println( (A+B)%C );
 		System.out.println( (A%C + B%C)%C );
 		System.out.println( (A*B)%C );
 		System.out.println( (A%C * B%C)%C );
-		
-	}
+	
+/*    
+굳이 String 변수 생성 안하고 입력과 동시에 구분자로 분리해줘도 된다.
  
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
+		System.out.println( (A+B)%C );
+		System.out.println( (A%C + B%C)%C );
+		System.out.println( (A*B)%C );
+		System.out.println( (A%C * B%C)%C );
+*/
+	}
 }
