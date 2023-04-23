ab13665674b9f92e7d37e7a8d5ae85837e16fd5a
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-04-23 23:10:10 +0900

    [Bronze V] Title: 영수증, Time: 224 ms, Memory: 17956 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -5,14 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt(); // 영수증에 적힌 총 금액
         int N = sc.nextInt(); // 종류의 수
-        int total = 0;
+        int sum = 0;
 
         for(int i = 0; i < N; i++){
             int a = sc.nextInt(); // 각 물건의 가격
             int b = sc.nextInt(); // 각 물건의 개수
-            total += a * b;
+            sum += a * b;
         }
-        if(total == X){
+        if(sum == X){
             System.out.println("Yes");
         }else{
             System.out.println("No");
